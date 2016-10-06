f76ac10564bb8aa86ad9d88279dfc8bfcb025c7b
AustinDev16 <austin.blaser@gmail.com>
2016-10-06 10:18:18 -0600

    Added a view controller for the list of entries. App persists events.

--- Event/AppDelegate.swift
+++ Event/AppDelegate.swift
@@ -24,15 +24,82 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print(EventController.sharedController.events.count)
         
         for event in EventController.sharedController.events {
+            print("Event name: \(event.name)")
             print("# of guests: \(event.guests.count)")
+            
             for guest in event.guests {
                 print((guest as! Guest).userName)
             }
+            
+            for toDoList in event.checklists {
+                let toDoList = toDoList as! Checklist
+                print("List name: \(toDoList.name)")
+                
+                for item in toDoList.listItems {
+                    let item = item as! ListItem
+                    print("Item: \(item.name)")
+                }
+            }
         }
         
+        //self.modelTests()
         
         return true
 
+        
+    }
+    
+    func modelTests(){
+        // create a user
+        
+        // create an event
+        
+        EventController.sharedController.addEvent(name: "Event 1")
+        
+        // create list(s)
+        let event = EventController.sharedController.events[1]
+        ChecklistController.sharedController.createNewCheckList(name: "To Do", event: event)
+        
+        ChecklistController.sharedController.createNewCheckList(name: "To Bring", event: event)
+        
+        // add item(s) to a checklist
+        let checklist = event.checklists[0] as! Checklist
+        
+        let newItem = ListItem(name: "Call caterer", responsibleParty: "Austin", checklist: checklist, event: event)
+        ChecklistController.sharedController.addItemToList(listItem: newItem, checklist: checklist)
+        
+        let newItem2 = ListItem(name: "Mow lawn", responsibleParty: "Austin", checklist: checklist, event: event)
+        ChecklistController.sharedController.addItemToList(listItem: newItem2, checklist: checklist)
+        
+        let newItem3 = ListItem(name: "Pick up dry cleaning", responsibleParty: "Austin", checklist: checklist, event: event)
+        ChecklistController.sharedController.addItemToList(listItem: newItem3, checklist: checklist)
+        
+        
+        
+        let checklist2 = event.checklists[1] as! Checklist
+        
+        let newItem4 = ListItem(name: "Potato Salad", responsibleParty: "Austin", checklist: checklist2, event: event)
+        ChecklistController.sharedController.addItemToList(listItem: newItem4, checklist: checklist2)
+        
+        
+        // invite others to event
+        
+        
+        
+        // complete a task
+        
+        // reassign a task
+        
+        // delete event
+        
+        
+        
+        // delete checklist
+        
+        // delete listItem
+        
+        
+        
         
     }
 
---
+++ Event/EventTableViewController.swift
@@ -0,0 +1,112 @@
+//
+//  EventTableViewController.swift
+//  Event
+//
+//  Created by Austin Blaser on 10/6/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewController: UITableViewController {
+
+    @IBAction func newEventTapped(_ sender: AnyObject) {
+        
+        let addEventAlertController = UIAlertController(title: "Event", message: "Fill out event details", preferredStyle: .alert)
+        
+        addEventAlertController.addTextField { (textField) in
+            textField.placeholder = "Event name"
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let add = UIAlertAction(title: "Create event", style: .default) { (_) in
+            
+            
+            guard let textField = addEventAlertController.textFields?.first,
+               let nameText = textField.text,
+            nameText.characters.count > 0 else { return }
+            textField.resignFirstResponder()
+            EventController.sharedController.addEvent(name: nameText)
+            
+            self.tableView.reloadData()
+        }
+        
+        
+        addEventAlertController.addAction(cancel)
+        addEventAlertController.addAction(add)
+        
+        self.present(addEventAlertController, animated: true, completion: nil)
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+ 
+    // MARK: - Table view data source
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return EventController.sharedController.events.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell", for: indexPath)
+
+        let event = EventController.sharedController.events[indexPath.row]
+        
+        cell.textLabel?.text = event.name
+        cell.detailTextLabel?.text = "\(event.detailDescription) \(event.date)"
+        return cell
+    }
+    
+
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
