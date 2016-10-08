83ed876cb52dc2ad03931dfe0dda56567b59da55
AustinDev16 <austin.blaser@gmail.com>
2016-10-08 13:38:00 -0600

    Added tableview to the invited guests view.

--- eventDetail_GuestsViewController.swift
+++ eventDetail_GuestsViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class eventDetail_GuestsViewController: UIViewController {
+class eventDetail_GuestsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     weak var delegate: InnerContentViewDelegate?
+    var guests: [Guest] {
+        guard let event = delegate?.event else {return []}
+        return event.guests.flatMap{ $0 as? Guest}
+    }
+    
+    var confirmedGuests: [Guest] {
+        return self.guests.filter { $0.hasAcceptedInvite == true }
+    }
+    
+    var unConfirmedGuests: [Guest] {
+        return self.guests.filter { $0.hasAcceptedInvite == false}
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.brown
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,15 +37,51 @@ class eventDetail_GuestsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        var sections = 1
+//        if confirmedGuests.count > 0 {
+//            sections += 1
+//        }
+        if unConfirmedGuests.count > 0 {
+            sections += 1
+        }
+        return sections
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return confirmedGuests.count
+        case 1: return unConfirmedGuests.count
+        default: return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "guestCell")
+        var guest: Guest?
+        
+        switch indexPath.section {
+        case 0: // Confirmed guests
+            guest = confirmedGuests[indexPath.row]
+        case 1: // Unconfirmed Guests
+            guest = unConfirmedGuests[indexPath.row]
+        default:
+            guest = nil
+        }
+        
+        cell?.textLabel?.text = guest?.userName
+        
+        
+        return cell ?? UITableViewCell()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0: return "Attending (\(confirmedGuests.count))"
+        case 1: return "Invited (\(unConfirmedGuests.count))"
+        default: return ""
+        }
+    }
 
 }
