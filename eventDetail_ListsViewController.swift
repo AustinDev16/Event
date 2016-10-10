aba8e3a6f77d5859c2de4005a360a22dc839c330
AustinDev16 <austin.blaser@gmail.com>
2016-10-10 11:53:48 -0600

    Added a table view to display checklists. Added custom ListItemTableViewCell, a delegate method via protocol.

--- eventDetail_ListsViewController.swift
+++ eventDetail_ListsViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class eventDetail_ListsViewController: UIViewController {
+class eventDetail_ListsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let tableView = UITableView()
     weak var delegate: InnerContentViewDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.red
+        
+        setupTableView()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    func setupTableView(){
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(tableView)
+        tableView.bounds = view.frame
+        tableView.delegate = self
+        
+        let tableViewTop = NSLayoutConstraint(item: tableView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1.0, constant: 0)
+        let tableViewLeading = NSLayoutConstraint(item: tableView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 0)
+        let tableViewTrailing = NSLayoutConstraint(item: tableView, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: 0)
+        let tableViewBottom = NSLayoutConstraint(item: tableView, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1.0, constant: 0)
+        self.view.addConstraints([tableViewTop, tableViewBottom, tableViewLeading, tableViewTrailing])
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,14 +45,34 @@ class eventDetail_ListsViewController: UIViewController {
     func newListButtonTapped(){
         print("new list button tapped")
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: TableView
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        guard let checklists = delegate?.event?.checklists else {return 0}
+        
+        return checklists.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let checklists = delegate?.event?.checklists,
+         let checklist = checklists[section] as? Checklist else {return 0}
+       
+        return checklist.listItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listItemCell") as! ListItemTableViewCell
+        guard let checklists = delegate?.event?.checklists,
+         let checklist = checklists[indexPath.section] as? Checklist else { return UITableViewCell() }
+     
+        let items = checklist.listItems.flatMap { $0 as? ListItem}
+        
+        cell.updateWithItem(item: items[indexPath.row])
+        cell.listItem = items[indexPath.row]
+        
+        return cell
+    }
+  
 
 }
