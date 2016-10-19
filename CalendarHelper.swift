605e66d71fc29ce5eabcb5575ba576ac3b5192ec
AustinDev16 <austin.blaser@gmail.com>
2016-10-19 14:30:42 -0600

    File renaming

---
+++ CalendarHelper.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarHelper.swift
+//  Event
+//
+//  Created by Austin Blaser on 10/19/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+import Foundation
+import EventKit
+
+extension Event {
+    convenience init(calEvent: EKEvent){
+        let name = calEvent.title
+        let date = calEvent.startDate as NSDate
+        let location = calEvent.location ?? ""
+        let detailDescription = calEvent.notes ?? ""
+        self.init(name: name, date: date, location: location, detailDescription: detailDescription)
+        self.calEventID = calEvent.eventIdentifier
+    }
+}
--- CalenderHelper.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CalenderHelper.swift
-//  Event
-//
-//  Created by Austin Blaser on 10/19/16.
-//  Copyright © 2016 Austin Blaser. All rights reserved.
-//
-
-import Foundation
